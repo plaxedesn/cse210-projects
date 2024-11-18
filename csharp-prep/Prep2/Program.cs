88e46cce5a4629fcc5c6ee1b8462b4e6a5d12f2d
plaxedesn <plaxvodloza@gmail.com>
2024-11-18 15:53:02 +0200

    week 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Give your grade percentage?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse (answer);
+
+        string letter = "";
+
+        if (percent >=90)
+        {
+            letter = "A";
+        }
+        else if (percent >=80)
+        {
+            letter = "B";
+        }
+        else if (percent >=70)
+        {
+            letter = "C";
+        }
+        else if (percent >=60)
+        {
+            letter = "C";
+        
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Grade:{letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats to you!");
+        }
+        else
+        {
+            Console.WriteLine("Try again next time");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+      Console.WriteLine("Hello Prep3 World!");
+      Random randomGenerator = new Random();
+      int magicNumber = randomGenerator.Next (1, 101);
+    
+    int guess = -1;
+
+    while (guess != magicNumber)
+    {
+        Console.Write("Guess a number between 1 and 100: ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+
+        }
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("That's it. Congrats!");
+        }
     }
+  }
 }

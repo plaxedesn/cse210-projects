ac96028cf590adbde4569b3b848504ec6e9f1cff
plaxedesn <plaxvodloza@gmail.com>
2024-11-25 15:46:37 +0200

    week2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Econet";
+        job1.startYear = 2014;
+        job1.endYear = 2022;
+
+        job1 job2 = new job1();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Netone";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = myResume ();
+        myResume._name = "Plaxedes Ncube";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }

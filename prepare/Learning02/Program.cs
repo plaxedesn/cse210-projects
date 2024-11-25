886c5435ea974033303aa27c9dc7c71cafa8a140
plaxedesn <plaxvodloza@gmail.com>
2024-11-25 16:45:24 +0200

    wEEK2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,23 +7,50 @@ class Program
         Console.WriteLine("Hello Learning02 World!");
 
         Job job1 = new Job();
-        job1._jobTitle = "Software Developer";
-        job1._company = "Econet";
-        job1.startYear = 2014;
-        job1.endYear = 2022;
+        job1._JobTitle = "Software Developer";
+        job1._Company = "Econet";
+        job1.StartYeartartYear = 2014;
+        job1.EndYear = 2022;
 
         job1 job2 = new job1();
-        job2._jobTitle = "Software Engineer";
-        job2._company = "Netone";
-        job2._startYear = 2023;
-        job2._endYear = 2024;
+        job2._JobTitle = "Software Engineer";
+        job2._Company = "Netone";
+        job2._StartYear = 2023;
+        job2._EndYear = 2024;
 
         Resume myResume = myResume ();
-        myResume._name = "Plaxedes Ncube";
+        myResume.Name = "Plaxedes Ncube";
 
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
+        myResume.Jobs.Add(job1);
+        myResume.Jobs.Add(job2);
 
         myResume.Display();
+    }
+}
+
+class Job
+{
+    public string JobTitle { get;set; }
+    public string Company {get; set;}
+    public int StartYear { get; set; }
+    public int EndYear { get; set;}
+}
+class Resume
+{
+    public string Name { get; set; }
+    public List<Job> Jobs { get; set; }
+
+    public Resume()
+    {
+        Jobs = new List<Job>();
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Resume of {Name}");
+        foreach (var job in Jobs)
+        {
+            Console.WriteLine($"{job.JobTitle} at {job.Company} ({job.StartYear} - {job.EndYear})");
+        }
     }
 }

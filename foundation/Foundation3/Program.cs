ea924260363bcced9ccba60e01cd6ed3752912ee
plaxedesn <plaxncube@gmail.com>
2024-12-16 01:08:06 +0200

    wk7

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        
+        List<Activity> activities = new List<Activity>
+        {
+            new RunningActivity(new DateTime(2022, 11, 3), 30, 3.0),
+            new CyclingActivity(new DateTime(2022, 11, 4), 45, 12.0),
+            new SwimmingActivity(new DateTime(2022, 11, 5), 30, 20)
+        };
+
+        
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
